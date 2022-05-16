5447c309de62eae159166f0f86655f3f879c6a7a
MarcZX10 <marczxrr@gmail.com>
2022-05-16 14:02:12 +0200

    formatear codigos

--- files/scripts/movewindow.js
+++ files/scripts/movewindow.js
@@ -5,67 +5,67 @@ var yPos = -100;
 var flagRun = 1;
 
 function changeTitle(title) {
-document.title = title;
+    document.title = title;
 }
 
 function openWindow(url) {
-aWindow = window.open(url, "_blank", 'menubar=no, status=no, toolbar=no, resizable=no, width=357, height=330, titlebar=no, alwaysRaised=yes');
+    aWindow = window.open(url, "_blank", 'menubar=no, status=no, toolbar=no, resizable=no, width=357, height=330, titlebar=no, alwaysRaised=yes');
 }
 
 function procreate() {
-changeTitle("Not a virus");
-for (var i = 0; i < 5; i++) {
-openWindow('4.html');
-}
+    changeTitle("Not a virus");
+    for (var i = 0; i < 5; i++) {
+        openWindow('4.html');
+    }
 }
 
 function altf4key() { if (event.keyCode == 18 || event.keyCode == 115) { alert("You are an idiot!"); procreate(); } }
 function ctrlkey() { if (event.keyCode == 17) { alert("You are an idiot!"); procreate(); } }
 function delkey() { if (event.keyCode == 46) { alert("You are an idiot!"); procreate(); } }
 
-function newXlt(){
-xOff = Math.ceil(-6 * Math.random()) * 5 - 10 ;
-window.focus()
+function newXlt() {
+    xOff = Math.ceil(-6 * Math.random()) * 5 - 10;
+    window.focus()
 }
 
-function newXrt(){
-xOff = Math.ceil(7 * Math.random()) * 5 - 10 ;
+function newXrt() {
+    xOff = Math.ceil(7 * Math.random()) * 5 - 10;
 }
 
-function newYup(){
-yOff = Math.ceil(-6 * Math.random()) * 5 - 10 ;
+function newYup() {
+    yOff = Math.ceil(-6 * Math.random()) * 5 - 10;
 }
 
-function newYdn(){
-yOff = Math.ceil( 7 * Math.random()) * 5 - 10 ;
+function newYdn() {
+    yOff = Math.ceil(7 * Math.random()) * 5 - 10;
 }
 
-function fOff(){
-flagrun = 0;
+function fOff() {
+    flagrun = 0;
 }
 
 function playBall() {
-xPos += xOff;
-yPos += yOff;
+    xPos += xOff;
+    yPos += yOff;
 
-if (xPos > screen.width - 357) {
-newXlt();
-}
+    if (xPos > screen.width - 357) {
+        newXlt();
+    }
 
-if (xPos < 0){
-newXrt();
-}
+    if (xPos < 0) {
+        newXrt();
+    }
 
-if (yPos > screen.height - 330) {
-newYup();
-}
+    if (yPos > screen.height - 330) {
+        newYup();
+    }
 
-if (yPos < 0){
-newYdn();
-}
+    if (yPos < 0) {
+        newYdn();
+    }
 
-if (flagRun == 1){
-window.moveTo(xPos, yPos);
-setTimeout('playBall()', 1);
-}
+    if (flagRun == 1) {
+        window.moveTo(xPos, yPos);
+        setTimeout('playBall()', 1);
+    }
 }
